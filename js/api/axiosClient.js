ed68b33903b2fe62eaa57d810c02eb992d9faadf
nguyencong <cnd.congnguyenduy@gmail.com>
2023-03-28 18:41:11 +0700

    render list post

--- js/api/axiosClient.js
+++ js/api/axiosClient.js
@@ -1,11 +1,35 @@
 import axios from 'axios';
 
+
+// Tạo 1 instance thay thế cho axios
 const axiosClient = axios.create({
-  baseURL: 'https://jsonplaceholder.typicode.com',
+  baseURL: 'https://js-post-api.herokuapp.com/api',
   timeout: 1000,
   headers: {
     'Content-Type': 'application/json'
   }
 });
+
+// Intercepter
+
+// Thêm một bộ đón chặn request
+axiosClient.interceptors.request.use(function (config) {
+    // Làm gì đó trước khi request dược gửi đi
+    return config;
+  }, function (error) {
+    // Làm gì đó với lỗi request
+    return Promise.reject(error);
+  });
+
+// Thêm một bộ đón chặn response
+axiosClient.interceptors.response.use(function (response) {
+    // Bất kì mã trạng thái nào nằm trong tầm 2xx đều khiến hàm này được trigger
+    // Làm gì đó với dữ liệu response
+    return response.data;
+  }, function (error) {
+    // Bất kì mã trạng thái nào lọt ra ngoài tầm 2xx đều khiến hàm này được trigger\
+    // Làm gì đó với lỗi response
+    return Promise.reject(error);
+  });
 
 export default axiosClient;
--- js/main.mjs
+++ js/main.mjs
@@ -1,12 +1,58 @@
 import postApi from "./api/postApi";
 
-async function getAll() {
-  const queryParams = {
-    _page: 1,
-    _limit: 3
+function createPostElement(post) {
+  if(!post) return;
+
+  try {
+    // find tempalte
+    const postTemplate = document.getElementById('postItemTemplate');
+    if(!postTemplate) return;
+
+    const liElement = postTemplate.content.firstElementChild.cloneNode(true);
+    if(!liElement) return;
+    // update title, content, thumbnail
+    const titleElement = liElement.querySelector('[data-id="title"]');
+    if(titleElement) titleElement.textContent = post.title;
+
+    const descriptionElement = liElement.querySelector('[data-id="description"]');
+    if(descriptionElement) descriptionElement.textContent = post.description;
+
+    const authorElement = liElement.querySelector('[data-id="author"]');
+    if(authorElement) authorElement.textContent = post.author;
+
+
+    const thumbnailElement = liElement.querySelector('[data-id="thumbnail"]');
+    if(thumbnailElement) thumbnailElement.src = post.imageUrl;
+
+    return liElement;
+  } catch (error) {
+    console.log("failed to create post item",error)
   }
-  const response = await postApi.getAll(queryParams);
-  console.log(response);
 }
 
-getAll();
+function renderPostList(postList) {
+  console.log({postList})
+  if(!Array.isArray(postList) || postList.length === 0) return;
+  
+  const ulElement = document.getElementById('postsList');
+  if(!ulElement) return;
+  postList.forEach((post) => {
+    const liElement = createPostElement(post);
+    console.log(liElement);
+
+    ulElement.appendChild(liElement);
+  })
+};
+
+(async () => {
+  try {
+    const queryParams = {
+      _page: 1,
+      _limit: 6
+    }
+    const { data , pagination} = await postApi.getAll(queryParams);
+    renderPostList(data);
+  } catch (error) {
+    console.log('get all error', error); 
+  }
+})();
