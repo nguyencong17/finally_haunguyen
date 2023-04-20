359e7f1340acc86fc8271d7a508e438a041930c0
nguyencong <cnd.congnguyenduy@gmail.com>
2023-04-20 17:05:02 +0700

    update

--- js/main.mjs
+++ js/main.mjs
@@ -1,7 +1,7 @@
 import postApi from './api/postApi';
 import {setTextContent, truncateText} from './utils';
 import dayjs from 'dayjs';
-import relativeTime from '/dayjs/plugin/relativeTime';
+import relativeTime from 'dayjs/plugin/relativeTime';
 
 //to use dayjs
 dayjs.extend(relativeTime)
@@ -33,30 +33,36 @@ function createPostElement(post) {
 }
 
 function renderPostList(postList) {
-  console.log({postList})
   if(!Array.isArray(postList) || postList.length === 0) return;
   
   const ulElement = document.getElementById('postsList');
   if(!ulElement) return;
   postList.forEach((post) => {
     const liElement = createPostElement(post);
-    console.log(liElement);
 
     ulElement.appendChild(liElement);
   })
 };
 
-function handlePrevClick() {
+function handleFilterChange(filterName, filterValue) {
+  const url = new URL(window.location);
+  url.searchParams.set(filterName, filterValue);
+  history.pushState({}, '', url);
+}
+
+function handlePrevClick(e) {
+  e.preventDefault();
   console.log('Prev Click');
 }
 
-function handleNextClick() {
+function handleNextClick(e) {
+  e.preventDefault();
   console.log('Next Click');
 }
 
 function initPagination() {
-  // bind event to button Next/Prev 
-  const ulPagination = getElementById('pagination');
+  // bind event to button Next/Prev
+  const ulPagination = document.getElementById('pagination');
   if(!ulPagination) return;
   
   const prevLink = ulPagination.firstElementChild?.firstElementChild;
@@ -64,23 +70,29 @@ function initPagination() {
     prevLink.addEventListener('click', handlePrevClick)
   }
 
+  const nextLink = ulPagination.lastElementChild?.firstElementChild;
   if(nextLink) {
     nextLink.addEventListener('click', handleNextClick)
   }
 }
 
+function initURL() {
+  const url = new URL(window.location);
+  if(!url.searchParams.get('_page')) url.searchParams.set('_page', 1);
+  if(!url.searchParams.get('_limit')) url.searchParams.set('_limit', 6);
+  history.pushState({}, '' , url);
+}
+
 (async () => {
   try {
-
-    initPagination();
-
-    const queryParams = {
-      _page: 1,
-      _limit: 6
-    }
-    const { data , pagination} = await postApi.getAll(queryParams);
+    initURL();
+    const queryParams = new URLSearchParams(window.location.search);
+    console.log(queryParams);
+    const { data, pagination } = await postApi.getAll(queryParams);
     renderPostList(data);
   } catch (error) {
     console.log('get all error', error); 
   }
+
+  initPagination();
 })();
